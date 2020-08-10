1c39db6d1f5a162a74dff3c0870c836f294009e2
abdulloooh <abdullahakinwumi@gmail.com>
2020-08-10 10:15:02 +0100

    completely scarped major album list

--- devqah/main.py
+++ devqah/main.py
@@ -2,44 +2,75 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen,Request
 from json5 import dumps
 
-base_url = "https://dawahnigeria.com/dawahcast/a"
-uri = "/189250" #this can be varied
+## getting album urls
+album_url = "https://dawahnigeria.com/dawahcast/"
 
-url = base_url+uri
-
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+req = Request(album_url, headers={'User-Agent': 'Mozilla/5.0'})
 urlClient = urlopen(req)
 html_content = urlClient.read()
 urlClient.close()
 
 page_soup = soup(html_content,"html.parser")
 
-albumTitle = page_soup.h1.text
-# print(albumTitle)
+wrappers = page_soup.findAll("section", {"id":"block-views-ramadan-1436-block-11"})
 
-wrappers = page_soup.findAll("div",{"class":"panel-pane pane-custom pane-3"})
-author = wrappers[0].div.h1.text
-# print(author)
+album_links = []
+for w in wrappers:
+  containers = w.findAll("td",{"class":"views-field views-field-title"})
+  for c in containers:
+    link = c.a['href']
+    album_links.append(link)
 
-container = page_soup.findAll("div", {"class":"jp-type-playlist"})[0].findAll("div", {"class":"jp-playlist"})[0]
-mediaContainer = container.findAll("li")
+##visiting each url and fetching contents
 
-media=[]
-for link in mediaContainer:
-    audioTitle = link.a.text
-    audioLink = (link.a['href'])[2:]
-    # print(audioTitle)
+base_url = "https://dawahnigeria.com"
+album = []
+i = 0
+for link in album_links:
+    url = base_url + link
+    try:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        urlClient = urlopen(req)
+        html_content = urlClient.read()
+        urlClient.close()
 
-    newAudio = {
-        "title": audioTitle,
-        "link" : audioLink
-    }
-    media.append(newAudio)
+        page_soup = soup(html_content,"html.parser")
 
-newAlbum = {
-    "title":    albumTitle,
-    "author":   author,
-    "media" :   media
-}
-# print(albumTitle)
-print(dumps((newAlbum)))
+        albumTitle = page_soup.h1.text
+        # print(albumTitle)
+
+        wrappers = page_soup.findAll("div",{"class":"panel-pane pane-custom pane-3"})
+        author = wrappers[0].div.h1.text
+        # print(author)
+
+        container = page_soup.findAll("div", {"class":"jp-type-playlist"})[0].findAll("div", {"class":"jp-playlist"})[0]
+        mediaContainer = container.findAll("li")
+
+        media=[]
+        for link in mediaContainer:
+            audioTitle = link.a.text
+            audioLink = (link.a['href'])[2:]
+            # print(audioTitle)
+
+            newAudio = {
+                "title": audioTitle,
+                "link" : audioLink
+            }
+            media.append(newAudio)
+
+        newAlbum = {
+            "title":    albumTitle,
+            "author":   author,
+            "media" :   media
+        }
+        # print(albumTitle)
+        # print(dumps((newAlbum)))
+        album.append(newAlbum)
+        i = i+1  ##just to count how many successful
+    except:
+        pass
+
+print (i)
+file = open("album.json", "w")
+file.write(dumps(album))
+file.close()
